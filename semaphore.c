507c90c3bc97748c1318dce88e0e55db0438a9c6
Andrew Lee <andrew.dragon.lee@gmail.com>
2016-11-03 12:33:18 -0400

    semaphores from recitation

--- semaphore.c
+++ semaphore.c
@@ -8,15 +8,26 @@ struct semaphore{
     int max;
     int active;
     struct spinlock lock;
-}
-
-typedef struct semaphore* semaphore;
-typedef struct spinlock* spinlock;
+};
 
 #define SEM_NUM 32
 
-semaphore SEM_STORE = semaphore[SEM_NUM];
-spinlock SEM_LOCK; 
+struct semaphore* SEM_STORE[SEM_NUM];
+
+void _sem_init(){
+    int n;
+
+    for(n = 0; n < SEM_NUM; n++){
+        SEM_STORE[semId] = (struct semaphore*) malloc(sizeof(*SEM_STORE[semId]));
+        SEM_STORE[semId]->lock = (struct spinlock*) malloc(sizeof(struct spinlock));
+
+        // "sem" + 2 characters for number + '\0'
+        char* lock_name = (char*) malloc(sizeof(char) * 6);
+        sprintf(lock_name, "sem%d", semId);
+
+        initlock(&(SEM_STORE[semId]->lock), lock_name);
+    }
+}
 
 int sem_init(int semId, int n){
     if(semId < 0 || semId >= SEM_NUM){
@@ -24,31 +35,16 @@ int sem_init(int semId, int n){
         return -1;
     }
 
-    acquire(&SEM_LOCK);
+    acquire(SEM_STORE[semId]->lock);
 
-    // Check to see if this semaphore has already been initialized.
     if(SEM_STORE[semId] && SEM_STORE[semId]->active){
-        release(&SEM_LOCK);
         return -1;
-    }
-
-    // Do we need to initialize the semaphore?
-    if(!SEM_STORE[semId]){
-        SEM_STORE[semId] = (semaphore) malloc(sizeof(*semaphore));
-        SEM_STORE[semId]->spinlock = (spinlock) malloc(sizeof(*spinlock));
     }
 
     SEM_STORE[semId]->value = SEM_STORE[semId]->max = n;
     SEM_STORE[semId]->active = 1;
-    SEM_STORE[semId]->spinlock = (spinlock) malloc(sizeof(*spinlock));
 
-    // "sem" + 2 characters for number + '\0'
-    char* lock_name = malloc(sizeof(char) * 6);
-    sprintf(lock_num, "sem%d", semId);
-
-    initlock(&(SEM_STORE[semId]->spinlock), lock_name);
-
-    release(&SEM_LOCK);
+    release(SEM_STORE[semId]->lock);
 
     return semId;
 }
@@ -58,33 +54,25 @@ int sem_destroy(int semId){
         return -1;
     }
 
-    acquire(&SEM_LOCK);
+    acquire(&SEM_STORE[semId]->lock);
 
     if(!SEM_STORE[semId] || !SEM_STORE[semId]->active){
-        release(&SEM_LOCK);
         return -1;
     }
 
     SEM_STORE[semId]->active = 0;
-    release(&(SEM_STORE[semId]->spinLock));
-
-    release(&SEM_LOCK);
 
     return semId;
 }
 
 // WARNING: ASSUMES YOU ALREADY HAVE A LOCK.
 int _can_acquire(int semId){
-    acquire(&SEM_LOCK);
-
     // Somebody messed up.
     if(!SEM_STORE[semId] || !SEM_STORE[semId]->active){
         panic("_can_acquire");
     }
 
     int current_value = SEM_STORE[semId]->value;
-
-    release(&SEM_LOCK);
 
     return current_value > 0;
 }
@@ -94,49 +82,59 @@ int sem_wait(int semId){
         return -1;
     }
 
-    acquire(&SEM_LOCK);
+    acquire(SEM_STORE[semId]->lock);
 
     if(!SEM_STORE[semId] || !SEM_STORE[semId]->active){
-        release(&SEM_LOCK);
         return -1;
     }
 
-    release(&SEM_LOCK);
-
     // We only want to wake up a single one.
-    while(_can_acquire()){
-        sleep(&semId, SEM_STORE[semId]->spinlock);
+    while(_can_acquire(semId)){
+        sleep(&semId, SEM_STORE[semId]->lock);
+        // when sleep returns we have the lock again
     }
 
-    // Prevent race conditions.
-    acquire(&SEM_LOCK);
-    SEM_STOqRE[semId]->value--;
-    release(&SEM_LOCK);
+    SEM_STORE[semId]->value--;
+
+    release(SEM_STORE[semId]->lock);
+
+    // semaphore is good (y)
 
     return semId;
 }
 
-void sem_signal(int semId){
+int sem_signal(int semId){
     if(semId < 0 || semId >= SEM_NUM){
         return -1;
     }
 
-    acquire(&SEM_LOCK);
+    acquire(SEM_STORE[semId]->lock);
 
     if(!SEM_STORE[semId] || !SEM_STORE[semId]->active){
-        release(&SEM_LOCK);
         return -1;
     }
 
     if(SEM_STORE[semId]->value == SEM_STORE[semId]->max){
-        release(&SEM_LOCK);
         return -1;
     }
 
     // It should be safe to acquire this since if we're calling
     // this we probably got past sem_wait.
     SEM_STORE[semId]->value++;
-    release(&SEM_LOCK);
+    wakeup(&semId);
 
-    wakeup(&usemId);
+    release(SEM_STORE[semId]->lock);
+}
+
+
+proc->chan = chan;
+if proc.chan == chan:
+    wake
+
+chan can just be the address of the semaphore being used
+
+sem_wait(sem_id){
+    // check the counter to see if we can lock the smeahphore
+
+    while not can take sem
 }
